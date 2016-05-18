c6500cad099b331215591e114589c3feb8ea236e
Mattias Olsson <mattias.olsson@hotmail.se>
2016-05-18 09:57:53 +0200

    clean startfil

--- Snake2/main.asm
+++ Snake2/main.asm
@@ -6,7 +6,34 @@
 ;
 
 
-; Replace with your application code
-start:
-    inc r16
-    rjmp start
+; [En lista med registerdefinitioner]
+/* t ex */
+.DEF rTemp				= r16
+.DEF rDirection			= r23
+/*…*/
+
+/* [En lista med konstanter] */
+/* t ex */
+.EQU NUM_COLUMNS		= 8
+.EQU MAX_LENGTH			= 25
+/* … */
+; [Datasegmentet]
+/* t ex */
+.DSEG
+matrix:		.BYTE 8
+snake:		.BYTE MAX_LENGTH+1
+/* … */
+; [Kodsegmentet]
+/* t ex */
+.CSEG
+// Interrupt vector table
+.ORG 0x0000
+	jmp init // Reset vector
+//... fler interrupts
+.ORG INT_VECTORS_SIZE
+init:
+	// Sätt stackpekaren till högsta minnesadressen
+	ldi rTemp, HIGH(RAMEND)
+	out SPH, rTemp
+	ldi rTemp, LOW(RAMEND)
+	out SPL, rTemp
